1b2941045d8b6de95a2510b2f8895bc89cfec057
Navneet Pal <navneetpal8106@gmail.com>
2024-11-01 17:05:14 +0530

    initial commit

---
+++ src/main/java/com/nav/java11features/Java11featuresApplication.java
@@ -0,0 +1,13 @@
+package com.nav.java11features;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java11featuresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java11featuresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nav/java11features/Java11featuresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nav.java11features;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java11featuresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
